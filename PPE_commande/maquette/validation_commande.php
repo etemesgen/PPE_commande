8469362cbdf882a103966f19702402d04cfa7220
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-04-15 16:29:22 +0200

    Update php script

--- PPE_commande/maquette/validation_commande.php
+++ PPE_commande/maquette/validation_commande.php
@@ -17,16 +17,16 @@
 <body>
     <main>
         <section>
-            <h2>Votre Commande du <?=$_SESSION['time']?></h2>
+            <h2>Votre Commande du <?php print $_SESSION['time']?></h2>
             <h3>Détail de votre commande:</h3>
-            <p>Nombre de produits: <?=$commande->getNbProduits();?></p>
+            <p>Nombre de produits: <?php print $commande->getNbProduits();?></p>
             <ul>
-                <li><?=$commande->getTablettes()?> : Tablettes</li>
-                <li><?=$commande->getPc()?> : PC</li>
-                <li><?=$commande->getPortable()?> : Portables</li>
+                <li><?php print $commande->getTablettes()?> : Tablettes</li>
+                <li><?php print $commande->getPc()?> : PC</li>
+                <li><?php print $commande->getPortable()?> : Portables</li>
             </ul>
-            <p>Le total de votre commande est de : <?=$commande->getPrix()?>€ (<?=$commande->getPrixHT()?>€ HT)</p>
-            <p>Adresse : <i><?=$commande->getAdresse();?></i></p>
+            <p>Le total de votre commande est de : <?php print $commande->getPrix()?>€ (<?=$commande->getPrixHT()?>€ HT)</p>
+            <p>Adresse : <i><?php print $commande->getAdresse();?></i></p>
         </section>
     </main>
 </body>
